175db3d5c6a687ac54c68a4c96fa8e43fb4d32ba
Yash Naravade <yashdip123@gmail.com>
2022-12-09 20:37:26 +0530

    Added colors

--- src/App.tsx
+++ src/App.tsx
@@ -8,61 +8,56 @@ function App() {
   return (
     <>
       <div className="container-fluid">
-      <div className="title-container">
+        <div className="title-container">
           <h1 className="heading mt-2">Emoji Kit 👻</h1>
         </div>
 
-<div className="row">
-      <div className="emoji-editor-container col-6">
-      <h3>Output:</h3>
-        <div className="main-emoji-container">
-
-
-          <div className="main-emoji">
-            
-            <div className="emoji">{emoji}</div>
-          </div>
-          <div className="emoji-container">
-            <div className="emoji" onClick={() => setEmoji("⚰️")}>
-              ⚰️
-            </div>
-
-            <div className="emoji" onClick={() => setEmoji("👻")}>
-              👻
-            </div>
-
-            <div className="emoji" onClick={() => setEmoji("🍑")}>
-              🍑
-            </div>
-
-            <div className="emoji" onClick={() => setEmoji("👀")}>
-              👀
-            </div>
-
-            <div className="emoji" onClick={() => setEmoji("🗿")}>
-              🗿
-            </div>
-
-            <div className="emoji" onClick={() => setEmoji("💀")}>
-              💀
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="control-editor col-6">
-      <h3>Input:</h3>
-        <div className="control-container">
-        <div>
-          hello
-        </div>
+        <div className="row">
+          <div className="emoji-editor-container col-6">
+            <h3>Output:</h3>
+            <div className="main-emoji-container">
+              <div className="main-emoji" style={{ backgroundColor: "maroon" }}>
+                <div className="emoji">{emoji}</div>
               </div>
-      </div>
+              <div className="emoji-container">
+                <div className="emoji" onClick={() => setEmoji("⚰️")}>
+                  ⚰️
+                </div>
 
+                <div className="emoji" onClick={() => setEmoji("👻")}>
+                  👻
+                </div>
 
+                <div className="emoji" onClick={() => setEmoji("🍑")}>
+                  🍑
+                </div>
 
+                <div className="emoji" onClick={() => setEmoji("👀")}>
+                  👀
+                </div>
 
-      </div>
+                <div className="emoji" onClick={() => setEmoji("🗿")}>
+                  🗿
+                </div>
+
+                <div className="emoji" onClick={() => setEmoji("💀")}>
+                  💀
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="control-editor col-6">
+            <h3>Input:</h3>
+            <div className="control-container">
+              <div className="color-item bg-red">Red</div>
+              <div className="color-item bg-purple">Purple</div>
+              <div className="color-item bg-pink">Pink</div>
+              <div className="color-item bg-orange">Orange</div>
+              <div className="color-item bg-green">Green</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
