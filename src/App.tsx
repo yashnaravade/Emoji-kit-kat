8772c88018529d1aa4989c85bf4d92060940d3dc
Yash Naravade <yashdip123@gmail.com>
2022-12-09 16:33:38 +0530

    Add emoji container and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="container-fluid">
-     <h1 className="heading mt-2">Emoji Kit 👻</h1>
-    </div>
-  )
+    <>
+      <div className="container-fluid">
+        <div className="title-container">
+          <h1 className="heading mt-2">Emoji Kit 👻</h1>
+        </div>
+      </div>
+
+      <div className="main-emoji-container">
+        <div className="main-emoji">
+          <div className="emoji">👻</div>
+          
+        </div>
+        <div className="emoji-container">
+          <div className="emoji">⚰️</div>
+          <div className="emoji">🍑</div>
+          <div className="emoji">👀</div>
+          <div className="emoji">🗿</div>
+          <div className="emoji">💀</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
